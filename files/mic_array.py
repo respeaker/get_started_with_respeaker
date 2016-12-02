8e15338e28dde5813c9ba4582fd3c8797682c733
Yihui Xiong <xiongyihui3@gmail.com>
2016-12-02 17:40:06 +0800

    Create mic_array.py

---
+++ files/mic_array.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+import respeaker.usb_hid as usb_hid
+
+
+class MicArray:
+    def __init__(self):
+        self.hid = usb_hid.get()
+
+    def write(self, address, data):
+        data = self.to_bytearray(data)
+        length = len(data)
+        if self.hid:
+            packet = bytearray([address & 0xFF, (address >> 8) & 0x7F, length & 0xFF, (length >> 8) & 0xFF]) + data
+            self.hid.write(packet)
+
+    def read(self, address, length):
+        self.hid.write(bytearray([address & 0xFF, (address >> 8) & 0xFF | 0x80, length & 0xFF, (length >> 8) & 0xFF]))
+        for _ in range(6):
+            data = self.hid.read()
+            # print [int(x) for x in data]
+            # skip VAD data
+            if int(data[0]) != 0xFF and int(data[1]) != 0xFF:
+                return data[4:(4 + length)]
+
+    @staticmethod
+    def to_bytearray(data):
+        if type(data) is int:
+            array = bytearray([data & 0xFF])
+        elif type(data) is bytearray:
+            array = data
+        elif type(data) is str:
+            array = bytearray(data)
+        elif type(data) is list:
+            array = bytearray(data)
+        else:
+            raise TypeError('%s is not supported' % type(data))
+
+        return array
+
+
+def main():
+    import sys
+    import struct
+
+    mic = MicArray()
+
+    if len(sys.argv) < 3:
+        print('Usage: python {} w 0x0 0x000003'.format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        if sys.argv[2].startswith('0x'):
+            address = int(sys.argv[2], 16)
+        else:
+            address = int(sys.argv[2])
+
+        if sys.argv[1] == 'w':
+            if sys.argv[3].startswith('0x'):
+                data = int(sys.argv[3], 16)
+            else:
+                data = int(sys.argv[3])
+
+            if data > 0xFFFF:
+                data = struct.pack('<I', data)
+            elif data > 0xFF:
+                data = struct.pack('<H', data)
+
+            mic.write(address, data)
+        else:
+            print [int(x) for x in mic.read(address, 4)]
+    except Exception as e:
+        print(e.message)
+
+
+if __name__ == '__main__':
+    main()
+
